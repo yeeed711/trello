000fc9a8018acec1210cacc41c071d31d565e411
yeeed711 <rladpwl910@naver.com>
2022-10-11 19:04:37 +0900

    feat: theme 추가

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,9 @@
 import { DefaultTheme } from 'styled-components';
 
 export const darkTheme: DefaultTheme = {
-  bgColor: '#0d1217',
-  borardColor: '#02040a',
-  cardColor: '#171a22',
+  bgColor: '#0d1117',
+  borardColor: '#010409',
+  cardColor: '#161b22',
   borderColor: '#252830',
   textColor: '#c9d1da',
 };
