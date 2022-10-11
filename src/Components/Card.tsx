a85a494b6d98d2f26e1156815fc8873816b5e1b7
yeeed711 <rladpwl910@naver.com>
2022-10-11 19:04:23 +0900

    feat: Card 구현

---
+++ src/Components/Card.tsx
@@ -0,0 +1,35 @@
+import { Draggable } from '@hello-pangea/dnd';
+import React from 'react';
+import styled from 'styled-components';
+
+interface ICardProps {
+  toDoId: number;
+  toDoText: string;
+  index: number;
+}
+
+const Card = ({ toDoId, toDoText, index }: ICardProps) => {
+  return (
+    <Draggable key={toDoId} draggableId={toDoId + ''} index={index}>
+      {(provided, snapshot) => (
+        <CardStyle
+          isDragging={snapshot.isDragging}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}>
+          {toDoText}
+        </CardStyle>
+      )}
+    </Draggable>
+  );
+};
+
+export default React.memo(Card);
+
+const CardStyle = styled.li<{ isDragging: boolean }>`
+  background-color: ${(props) => props.theme.cardColor};
+  border-radius: 8px;
+  padding: 10px;
+  font-size: 14px;
+  line-height: 21px;
+`;
